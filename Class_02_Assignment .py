20f79a90267a1c652feb589c9406de82f2d38ba1
Md. Mahmodul Hassen Khan Shadin <shadhinkhan28@gmail.com>
2024-10-07 11:02:47 +0600

    Add files via upload

---
+++ Class_02_Assignment .py
@@ -0,0 +1,65 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('HI')
+
+print('Hello')
+
+a = "Hello World!"
+
+print(a)
+
+# 1. Print your name.
+
+print("My Name is Md. Mahmodul Hassen Khan Shadin")
+
+# 2. 2. Input your batch number using input() function.
+
+str(input("Input Your Batch Number Here "))
+
+# 3. Create 5 different valid Variables and print.
+
+camelCaseVariable = "I am a Django Developer"
+PascalCaseVariable = "I am a Django Developer"
+snake_case_variable = "I am a Django Developer"
+Batch_10 = 'I am a Django Developer'
+___Django = "I am a Django Developer"
+
+print(camelCaseVariable)
+print(PascalCaseVariable)
+print(snake_case_variable)
+print(Batch_10)
+print(___Django)
+
+# 4. Create Pascal Case Variable.
+
+traineeName = "Shadhin Khan"
+print(traineeName)
+
+# 5. Assign a single value to Multiple variables.
+
+a = b = c = d = e = 10
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# 6. Input float value using input().
+
+float(input('Enter a float value here '))
+
+# 7. a = ‘Welcome to Django for web and Ai’ convert into upper case.
+
+a = 'Welcome to Django for web and Ai'
+print(a.upper())
+
+# 8. b = ‘ Python’remove whitespace.
+
+b = '  Python'
+print(b.split())
+
+
